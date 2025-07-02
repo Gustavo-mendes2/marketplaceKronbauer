4445c730e7584dc8467f42b2ba7bac064007d8e0
Gustavo Mendes <gustavo568telesmendes@gmail.com>
2025-07-02 00:07:48 -0300

    Testando se funciona

---
+++ marketplaceKronbauer/src/serviços/ServiçoPagamentoCrédito.java
@@ -0,0 +1,26 @@
+package serviços;
+
+import entidades.Conta;
+import entidades.enumeradas.Tipo;
+
+public class ServiçoPagamentoPaypal implements ServiçoPagamento {
+    @Override
+    public double Pagamento(Conta conta, Double PagamentoTotal) {
+        if (conta.getTier() == Tipo.BRONZE) {
+            return PagamentoTotal * 1.20;
+        }
+        if (conta.getTier() == Tipo.PRATA) {
+            return PagamentoTotal * 1.10;
+        }
+        if (conta.getTier() == Tipo.OURO) {
+            return PagamentoTotal * 1;
+        }
+        if (conta.getTier() == Tipo.PLATINA) {
+            return PagamentoTotal * 0.90;
+        }
+        if (conta.getTier() == Tipo.DIAMANTE) {
+            return PagamentoTotal * 0.80;
+        }
+        return 0;
+    }
+}
---
+++ marketplaceKronbauer/src/serviços/ServiçoPagamentoPix.java
@@ -0,0 +1,27 @@
+package serviços;
+
+import entidades.Conta;
+import entidades.enumeradas.Tipo;
+
+public class ServiçoPagamentoPix implements ServiçoPagamento{
+
+  @override
+  public double Pagamento(Conta conta, Double PagamentoTotal) {
+      if (conta.getTier() == Tipo.BRONZE){
+          return PagamentoTotal * 1.20;
+      }
+      if(conta.getTier() == Tipo.PRATA){
+          return PagamentoTotal * 1.10;
+      }
+      if(conta.getTier() == Tipo.OURO){
+          return PagamentoTotal * 1;
+      }
+      if(conta.getTier() == Tipo.PLATINA){
+          return PagamentoTotal * 0.90;
+      }
+      if (conta.getTier() == Tipo.DIAMANTE){
+          return PagamentoTotal * 0.80;
+      }
+      return 0;
+  }
+}
